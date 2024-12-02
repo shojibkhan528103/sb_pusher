9f3767fc0dd6dfa56a6fc55517642b10ab9b0df5
shojibkhan528102 <shojibkhan.dev@gmail.com>
2024-12-02 23:28:45 +0600

    first commit

--- app/Http/Controllers/pusherCheckController.php
+++ app/Http/Controllers/pusherCheckController.php
@@ -5,7 +5,6 @@ namespace App\Http\Controllers;
 use App\Models\pusherone;
 use App\Models\pushertow;
 use Illuminate\Http\Request;
-use Laravel\Reverb\Events\MessageSent;
 
 class pusherCheckController extends Controller
 {
@@ -14,11 +13,9 @@ class pusherCheckController extends Controller
         $pusherone->firstname_pushone = $request->firstName_push_one;
         $pusherone->lastname_pushone = $request->lastName_push_one;
         $pusherone->status = 1;
-        $message = $request->firstName_push_one;
-        $additionalData = $request->lastName_push_two;
         $save_one = $pusherone->save();
         if ($save_one) {
-            broadcast(new MessageSent($message, $additionalData))->toOthers();
+            return back()->with('status', 'successful pusher one');
         }
         return back()->with('status', 'Fail to save data puser one');
     }
@@ -27,12 +24,9 @@ class pusherCheckController extends Controller
         $pushertow->firstname_pushtwo = $request->firstName_push_two;
         $pushertow->lastname_pushtwo = $request->lastName_push_two;
         $pushertow->status = 1;
-        $message = $request->firstName_push_two;
-        $additionalData = $request->lastName_push_two;
         $save_one = $pushertow->save();
         if ($save_one) {
-            broadcast(new MessageSent($message, $additionalData))->toOthers();
-            return response()->json(['status' => 'Message broadcasted! and successful data pusher two']);
+            return back()->with('status', 'successful data pusher two');
         }
         return back()->with('status', 'Fail to save data pusher two');
     }
